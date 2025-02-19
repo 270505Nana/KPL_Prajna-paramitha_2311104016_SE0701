4c092a0de0d58099d5e2436195f05d83ddad9e87
270505Nana <prajna270505@gmail.com>
2025-02-19 12:37:45 +0700

    add : Challenge identitas

---
+++ 01_Pengantar_Praktikum_KPL/identitas.js
@@ -0,0 +1,38 @@
+// import process from "process";
+// import readline from "readline"; //buat baca barisnya
+
+// const input = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+
+// input.question("Masukkan nama anda: ", (nama) => {
+//     // nama sebagai call back dengan nama var ano
+//     console.info(`Hallo ${nama}`)
+
+//     input.close();
+// })
+
+
+import process from "process";
+import readline from "readline";
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+input.question("Siapa nama kamu?" , (nama) =>{
+    console.info(Hallo ${nama})
+input.question("Apa jurusanmu?" , (jurusan) =>{
+    console.info(jurusan: ${jurusan})
+input.question("Apa nama universitasmu?" , (universitas) =>{
+    console.info(universitas: ${universitas})
+input.question("berapa umurmu?" , (umur) =>{
+    console.info(umur : ${umur})
+    
+    
+    input.close();
+})
+})
+}) 
+})
---
+++ 01_Pengantar_Praktikum_KPL/os.js
@@ -0,0 +1,10 @@
+import os from "os";
+
+console.info(os.platform());
+console.info(os.arch());
+console.info(os.cpus());
+console.info(os.uptime());
+console.info(os.platform());
+console.info(os.totqlmem());
+console.info(os.freemen());
+console.table(os.networkInterfaces());
